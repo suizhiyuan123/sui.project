4270f51f4087e96f74461994869c4b12a51f91fc
6kongxiangrui <1910437078@qq.com>
2025-07-02 13:36:11 +0800

    kong-v1

--- rs_interpret.m
+++ rs_interpret.m
@@ -1,10 +1,14 @@
-%得到码距最短的码字：
-function  interpret_msg=rs_interpret(in_msg,all_code_msg,all_msg)
-for i=1:512
-    rs_dis(i)=rscode_dis(in_msg,all_code_msg(i,:));    
+function  interpret_msg = rs_interpret(in_msg, all_code_msg, all_msg)
+    % 计算输入消息与所有编码消息之间的距离
+    rs_dis = zeros(1, size(all_code_msg, 1));
+    for i = 1:size(all_code_msg, 1)
+        rs_dis(i) = rscode_dis(in_msg, all_code_msg(i,:));    
+    end
+    
+    % 找出最小距离和对应的索引
+    rs_dis_min = min(rs_dis);
+    index = find(rs_dis == rs_dis_min);
+    
+    % 返回第一个匹配的原始消息
+    interpret_msg = all_msg(index(1), :);
 end
-rs_dis_min =min(rs_dis);
-index = find(rs_dis == rs_dis_min);
-interpret_msg = all_msg(index(1),:);
- 
-return;
